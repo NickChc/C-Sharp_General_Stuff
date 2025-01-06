535c553dd9ec748732c8947c6de272386e9563fc
NickChc <nika.chichua26@gmail.com>
2025-01-06 17:53:34 +0400

    RegionInfo class

---
+++ saved/RegionInfo.cs
@@ -0,0 +1,15 @@
+// using System.Globalization;
+
+// namespace RegionInfoLesson
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             RegionInfo info = new("GE");
+
+//             Console.WriteLine(info.DisplayName);
+//             Console.WriteLine(info.IsMetric);
+//         }
+//     };
+// }
