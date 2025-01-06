2e7c1b34a9af7b9b8e345d928d03638996d482e1
NickChc <nika.chichua26@gmail.com>
2025-01-06 16:43:14 +0400

    Preprocessor directives

---
+++ saved/PreprocessorDirectives.cs
@@ -0,0 +1,27 @@
+// #define UnderDev
+// #pragma warning disable
+
+// namespace PreprocessorDirectives
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             #region Env check
+// #if(UnderDev)
+//             Console.WriteLine("App under dev");
+//             // #error The app is under development
+// #else
+//             Console.WriteLine("App in Production");
+// #warning This app is complete and ready to be published
+// #endif
+//             #endregion
+
+//             #region Declarations
+
+//             var name = "";
+
+//             #endregion
+//         }
+//     };
+// }
