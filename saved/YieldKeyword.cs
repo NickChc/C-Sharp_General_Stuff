f5e6f999d3333c85c55e5c163d939ef117b98a54
NickChc <nika.chichua26@gmail.com>
2025-01-08 17:14:57 +0400

    yield keyword

---
+++ saved/YieldKeyword.cs
@@ -0,0 +1,24 @@
+// using System.Security.Cryptography.X509Certificates;
+
+// namespace YieldKeyword
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             foreach (var item in PerformMathOps(3, 2))
+//             {
+//                 Console.WriteLine(item);
+//             }
+//         }
+
+//         public static IEnumerable<double> PerformMathOps(int x, int y)
+//         {
+//             yield return x + y;
+//             yield return x - y;
+//             yield return x * y;
+//             yield return x / y;
+//             yield return x % y;
+//         }
+//     };
+// }
